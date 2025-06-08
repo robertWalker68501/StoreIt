781576ea747d15e5ee11bdd9552ef90eae5235e1
robertWalker68501 <robert.walker6580@gmail.com>
2025-06-08 11:52:18 -0700

    first commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,5 @@
+import AuthForm from '@/components/AuthForm';
+
+const SignInPage = () => <AuthForm type='sign-in' />;
+
+export default SignInPage;
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+  return (
+    <div className='flex-center h-screen'>
+      <h1 className='text-3xl font-bold'>
+        StoreIt - The only storage solution you need
+      </h1>
+    </div>
+  );
+};
+
+export default Page;
